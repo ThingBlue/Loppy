348a0b4312820717bc10909e8fcba6952274b4d3
ThingBlue <thingbluex@gmail.com>
2023-05-18 13:52:03 -0400

    Level generation now always succeeds in one attempt if possible
    Trace back mechanic

--- Assets/Scripts/Level/LevelGenerator.cs
+++ Assets/Scripts/Level/LevelGenerator.cs
@@ -12,6 +12,9 @@ namespace Loppy.Level
     public class RoomDataNode
     {
         public string type;
+        public RoomData roomData;
+        public Vector2 roomCenter;
+        public GameObject gameObject = null;
         public List<RoomDataNode> children;
     }
 
@@ -27,15 +30,11 @@ namespace Loppy.Level
         public int randomSeed;
 
         private Dictionary<string, Dictionary<string, List<RoomData>>> rooms;
-        private List<KeyValuePair<RoomData, Vector2>> generatedRooms;
-        private List<GameObject> generatedRoomGameObjects;
 
         private void Start()
         {
             // Initialize storage
             rooms = new Dictionary<string, Dictionary<string, List<RoomData>>>();
-            generatedRooms = new List<KeyValuePair<RoomData, Vector2>>();
-            generatedRoomGameObjects = new List<GameObject>();
 
             // Fetch and initialize rooms
             if (!initializeRooms()) Debug.Log("Could not initialize rooms!");
@@ -69,27 +68,29 @@ namespace Loppy.Level
         // Repeatedly attempts to generate the level
         // Returns true if level generation successful
         // Returns false if level generation fails 10 times
-        public bool generateLevel(RoomDataNode levelData, string region)
+        public bool generateLevel(RoomDataNode root, string region)
         {
             int failureCount = 0;
 
             // Try to generate level
             while (failureCount < 10)
             {
-                if (generateRoom(levelData, Vector2.zero, EntranceDirection.LEFT, region)) return true;
+                //if (generateRoom(levelData, Vector2.zero, EntranceDirection.LEFT, region)) return true;
+                if (generateRoom(root, Vector2.right * failureCount * 5, EntranceDirection.LEFT, region, root)) return true;
 
-                deleteGeneratedRooms();
+                //deleteGeneratedRooms();
                 failureCount++;
+                //randomSeed++;
             }
             return false;
         }
 
         // Recursively generates rooms using the level data node given
-        private bool generateRoom(RoomDataNode node, Vector2 entrancePosition, EntranceDirection entranceDirection, string region)
+        private bool generateRoom(RoomDataNode node, Vector2 entrancePosition, EntranceDirection entranceDirection, string region, RoomDataNode root)
         {
             if (entranceDirection == EntranceDirection.NONE) return false;
 
-            UnityEngine.Random.InitState(randomSeed);
+            //UnityEngine.Random.InitState(randomSeed);
 
             // Randomly generate room
             List<int> usedIndices = new List<int>();
@@ -112,14 +113,16 @@ namespace Loppy.Level
                 // Check if entrance direction matches exit direction of last room
                 if (roomData.entranceDirection != entranceDirection) continue;
                 // Check if room overlaps any existing rooms
-                if (checkRoomOverlap(roomData, roomCenter)) continue;
+                if (checkRoomOverlap(roomData, roomCenter, root)) continue;
 
                 // Room fits, generate it
                 GameObject newRoomGameObject = Instantiate(rooms[region][node.type][randomIndex].roomPrefab, roomCenter, Quaternion.identity);
-                generatedRooms.Add(new KeyValuePair<RoomData, Vector2>(roomData, roomCenter));
-                generatedRoomGameObjects.Add(newRoomGameObject);
+                node.gameObject = newRoomGameObject;
+                node.roomData = roomData;
+                node.roomCenter = roomCenter;
 
                 // Recurse
+                bool childrenSuccess = true;
                 for (int i = 0; i < node.children.Count; i++)
                 {
                     // Calculate position of exit for current room
@@ -133,41 +136,64 @@ namespace Loppy.Level
                     if (roomData.exitDirections[i] == EntranceDirection.BOTTOM) nextEntranceDirection = EntranceDirection.TOP;
 
                     // Return false if any children fail
-                    if (!generateRoom(node.children[i], exitPosition, nextEntranceDirection, region)) return false;
+                    if (!generateRoom(node.children[i], exitPosition, nextEntranceDirection, region, root))
+                    {
+                        childrenSuccess = false;
+                        break;
+                    }
                 }
 
                 // All recursions successful, return true
-                return true;
+                if (childrenSuccess) return true;
+
+                // Try again if any children fail
+                deleteGeneratedRooms(node);
+                node.gameObject = null;
             }
 
             // No suitable rooms found
             return false;
         }
 
-        private bool checkRoomOverlap(RoomData roomData, Vector2 roomCenter)
+        private bool checkRoomOverlap(RoomData roomData, Vector2 roomCenter, RoomDataNode node)
         {
-            // Check if room overlaps any previous rooms
-            bool overlap = false;
-            foreach (KeyValuePair<RoomData, Vector2> existingRoom in generatedRooms)
+            // Check if current node has no instantiated room
+            if (!node.gameObject) return false;
+
+            // Check for overlap with current room node
+            if (Mathf.Abs(roomCenter.x - node.roomCenter.x) * 2 < (roomData.size.x + node.roomData.size.x) &&
+                Mathf.Abs(roomCenter.y - node.roomCenter.y) * 2 < (roomData.size.y + node.roomData.size.y))
             {
-                if (Mathf.Abs(roomCenter.x - existingRoom.Value.x) * 2 < (roomData.size.x + existingRoom.Key.size.x) &&
-                    Mathf.Abs(roomCenter.y - existingRoom.Value.y) * 2 < (roomData.size.y + existingRoom.Key.size.y))
+                return true;
+            }
+
+            // Recurse
+            bool childrenOverlap = false;
+            foreach (RoomDataNode child in node.children)
+            {
+                if (checkRoomOverlap(roomData, roomCenter, child))
                 {
-                    overlap = true;
+                    childrenOverlap = true;
                     break;
                 }
             }
-            return overlap;
+
+            return childrenOverlap;
         }
 
-        private void deleteGeneratedRooms()
+        private void deleteGeneratedRooms(RoomDataNode node)
         {
-            // Delete all generated room game objects
-            foreach (GameObject roomGameObject in generatedRoomGameObjects) Destroy(roomGameObject);
+            // End recursion if current node has no instantiated room
+            if (!node.gameObject) return;
 
-            // Clear lists
-            generatedRooms.Clear();
-            generatedRoomGameObjects.Clear();
+            // Delete room of current node
+            if (node.gameObject) Destroy(node.gameObject);
+            node.gameObject = null;
+            node.roomData = null;
+            node.roomCenter = Vector2.zero;
+
+            // Recursively destroy children rooms
+            foreach (RoomDataNode child in node.children) deleteGeneratedRooms(child);
         }
     }
 }
